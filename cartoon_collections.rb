876219b95300346b8545871ffce236165c9d4a3b
Kish Siva <rodes88@msn.com>
2019-04-09 23:38:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,15 +23,3 @@ def find_the_cheese(cheese)
   end
   
 end
-
-describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      cheddar_cheese = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(cheddar_cheese)).to eq 'cheddar'
-    end
-
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
-      expect(find_the_cheese(no_cheese)).to eq nil
-    end
-  end
